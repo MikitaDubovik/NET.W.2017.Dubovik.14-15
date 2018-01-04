9aa472854060097c1d6ca7cd5e4255dcad08017f
Никита Дубовик <nikitomas@tut.by>
2018-01-05 01:34:59 +0300

    First step of authentication

---
+++ NET.W.2017.Dubovik.14-15/PL.ASP.NET_MVC/Controllers/InformationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.ASP.NET_MVC.Controllers
+{
+    public class InformationController : Controller
+    {
+        // GET: Information
+        /// <summary>
+        /// Shows contacts for communication
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Contact() => View();
+
+        /// <summary>
+        /// Shows information about this project
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult About() => View();
+    }
+}
